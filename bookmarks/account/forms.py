4ba609b259e8180bfbd2a02b3e6d42d82e572e3b
Sobhan Zadehali <99137872+soza780@users.noreply.github.com>
2024-03-22 11:14:07 +0330

    profile form

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import Profile
 
 
 class LoginForm(forms.Form):
@@ -14,10 +15,22 @@ class UserRegistrationForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ["username", "first_name", "email"]
-    
+
     def cleaned_password2(self):
         cd = self.cleaned_data
         if cd['password'] != cd["password2"]:
-            raise forms.ValidationError("password does't match!")
+            raise forms.ValidationError("password doesn't match!")
         else:
             return cd["password2"]
+
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ["date_of_birth", "photo"]
+
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['email', 'first_name', 'last_name']
