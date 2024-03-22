9c67eb30776de9da009079a8682d526ff1c915c2
Sobhan Zadehali <99137872+soza780@users.noreply.github.com>
2024-03-22 14:22:12 +0330

    minor

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -43,7 +43,7 @@ SOCIAL_AUTH_PIPELINE = [
     'social_core.pipeline.social_auth.social_user',
     'social_core.pipeline.user.get_username',
     'social_core.pipeline.user.create_user',
-    'account.authentication.create_profile',
+    'account.authentication.create_profile', # creates user profile in social core pipline
     'social_core.pipeline.social_auth.associate_user',
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
